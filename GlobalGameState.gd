43c3b72c6d1f01b5699410e84db4d4dcccd2f3a8
Xalrandion <alexgotte2@gmail.com>
2024-12-14 14:59:59 +0100

    UP global game state

--- GlobalGameState.gd
+++ GlobalGameState.gd
@@ -1,22 +1,4 @@
 extends Node
 
-@export var trap_inventory = {};
-
-func removeTrap(name: String) -> bool:
-	assert(trap_inventory.find_key(name), "Trap does not exists")
-	if trap_inventory[name] == 0:
-		print("Trying to get a trap that have 0 left")
-		return false
-	trap_inventory[name] -= 1;
-	return true
-
-
-func addTrap(name: String):
-	assert(trap_inventory.find_key(name), "Trap does not exists")
-	trap_inventory[name] += 1;
-	
-func trap_count(name: String) -> int:
-	assert(trap_inventory.find_key(name), "Trap does not exists")
-	return trap_inventory[name]
-
+@export var available_traps: Array
 @export var number_of_coin: int = 0;
