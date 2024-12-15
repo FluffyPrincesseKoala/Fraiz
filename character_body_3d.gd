b9f78e7cf921df4033d89003ccf82121f759dbdc
Jimmy Zahgbib <cyrillemaire1@gmail.com>
2024-12-15 13:20:32 +0100

    pomme

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -20,16 +20,20 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var bump_velocity: Vector3 = Vector3.ZERO
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if bump_velocity.length() > 0:
+		velocity += bump_velocity
+		bump_velocity = bump_velocity.move_toward(Vector3.ZERO, 20 * delta)  # Gradual slowdown
 
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -42,3 +46,8 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+# Function to handle getting bumped by traps
+func get_bumped(force: Vector3):
+	print("Character got bumped with force:", force)
+	bump_velocity = force
